3798948713aff3bf9741198dd54b591d58954f16
Oskar Järviniemi <oskar.jarviniemi@hotmail.com>
2018-01-30 16:12:05 +0100

    doing stuff

--- Js/main.js
+++ Js/main.js
@@ -3,10 +3,6 @@ let matchedCards = [];
 let firstCard = null;
 let flippedCards = [];
 
-const color = ['#ffce00','#ffce00', '#0099ff','#0099ff','#191995','#191995','#ff0000', '#ff0000','#40a521','#40a521', '#001a9c','#001a9c', '#8b009c', '#8b009c','#9c3700','#9c3700', '#746bdf','#746bdf', '#dfc86b','#dfc86b'];
-let memoryValues = [];
-let memoryId = [];
-let flippedTile= 0;
 
 function shuffle(a) {
   var j, x, i;
