a0b1f6c7fe590bb2b1c8987d4e8406d8c78478b6
Oskar Järviniemi <oskar.jarviniemi@hotmail.com>
2018-01-30 16:11:54 +0100

    fixed stuff

--- Js/main.js
+++ Js/main.js
@@ -1,3 +1,7 @@
+let colors = ['#ffce00','#ffce00', '#0099ff','#0099ff','#191995','#191995','#ff0000','#ff0000', '#40a521', '#40a521','#000000','#000000', '#8b009c','#8b009c', '#9c3700','#9c3700', '#746bdf','#746bdf', '#dfc86b', '#dfc86b'];
+let matchedCards = [];
+let firstCard = null;
+let flippedCards = [];
 
 const color = ['#ffce00','#ffce00', '#0099ff','#0099ff','#191995','#191995','#ff0000', '#ff0000','#40a521','#40a521', '#001a9c','#001a9c', '#8b009c', '#8b009c','#9c3700','#9c3700', '#746bdf','#746bdf', '#dfc86b','#dfc86b'];
 let memoryValues = [];
